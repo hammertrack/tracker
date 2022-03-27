c0a5c2ea5bff8f9691c3dcbda67175eb941e07b2
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2022-03-27 23:29:53 +0200

    fix typo

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,7 +2,7 @@ package utils
 
 import "unsafe"
 
-// byte to string without allocation. Use with care and make sure to not modify
+// byte to string without allocation. Use with care and make sure not to modify
 // the byte buffer while using the resulting string
 func ByteToStr(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
