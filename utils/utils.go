5d78e1f1d0ea6c25d7b7840cac78f6f55ab687ba
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2022-04-02 22:22:27 +0200

    add StrToByte to utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,23 @@
 package utils
 
-import "unsafe"
+import (
+	"reflect"
+	"unsafe"
+)
 
 // byte to string without allocation. Use with care and make sure not to modify
 // the byte buffer while using the resulting string
 func ByteToStr(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
+}
+
+// String to byte without allocation. Use with care and make sure not to modify
+// the provided string while using the resulting byte slice
+func StrToByte(s string) (b []byte) {
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Cap = sh.Len
+	bh.Len = sh.Len
+	return b
 }
